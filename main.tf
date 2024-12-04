40204edb85bec2f6d4c1be12ef0f9495724cb6db
jainsomya <cs.jainsomya@gmail.com>
2024-12-04 11:06:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 resource "aws_s3_bucket" "bucket1" {
-	bucket = "somya-first-bucket"
+	bucket = "neeha-first-bucket"
 }
 
-resource "aws_s3_bucket" "bucket2" {    
+resource "aws_s3_bucket" "bucket2" {
+    
 }
 
 provider "aws" {
@@ -11,10 +12,10 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "somya-s3bucket-0412"
-    key            = "somya/s3/terraform.tfstate"
+    bucket         = "neeha-s3bucket-0412"
+    key            = "neeha/s3/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "somya-0412-dblocks"
+    dynamodb_table = "neeha-0412-dblocks"
     encrypt        = true
   }
 }
